d25e1bb440b18ca49f6aca91dd8094509c201762
alupoaeiandreea <andreeaalupoaei@yahoo.com>
2018-06-05 16:18:49 +0300

    Binding component properties

--- angular-essentials/src/app/user.component.ts
+++ angular-essentials/src/app/user.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  template: '<input type="text" (input)="onUserInput($event)" [value]="name"><p>Hello {{ name }}!</p>'
-  // Event binding ( (input) ), property binding ( [value] ) and string interpolation ( {{name}} )
+  template: '<input type="text" [(ngModel)]="name"><p>Hello {{ name }}!</p>'
 })
 export class UserComponent {
-  name = 'Max';
+  @Input() name = 'Max';
 
   onUserInput(event) {
     this.name = event.target.value;
